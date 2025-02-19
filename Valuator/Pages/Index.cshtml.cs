ad7e8f3d9a43e71dfdb740e380aff0337784aa5d
DESKTOP-K55KTC9\Prost <prosto_channal@mail.ru>
2025-02-19 09:49:22 +0300

    Добавил вычисление, сохранение и заргузку данных из бд

--- Valuator/Pages/Index.cshtml.cs
+++ Valuator/Pages/Index.cshtml.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StackExchange.Redis;
 
 namespace Valuator.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConnectionMultiplexer _redis;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConnectionMultiplexer redis)
     {
         _logger = logger;
+        _redis = redis;
     }
 
     public void OnGet()
@@ -21,17 +24,60 @@ public class IndexModel : PageModel
     {
         _logger.LogDebug(text);
 
+        IDatabase db = _redis.GetDatabase();
+
         string id = Guid.NewGuid().ToString();
 
         string textKey = "TEXT-" + id;
         // TODO: (pa1) сохранить в БД (Redis) text по ключу textKey
+        db.StringSet(textKey, text);
 
         string rankKey = "RANK-" + id;
         // TODO: (pa1) посчитать rank и сохранить в БД (Redis) по ключу rankKey
+        double rank = CalculateRank(text);
+        db.StringSet(rankKey, rank);
 
         string similarityKey = "SIMILARITY-" + id;
         // TODO: (pa1) посчитать similarity и сохранить в БД (Redis) по ключу similarityKey
+        double similarity = CalculateSimilarity(db, text, textKey);
+        db.StringSet(similarityKey, similarity);
 
         return Redirect($"summary?id={id}");
+    }
+
+    private double CalculateRank(string text)
+    {
+        double count = 0;
+
+        foreach (char ch in text)
+        {
+            if (!char.IsLetter(ch))
+            {
+                count++;
+            }
+        }
+
+        return count / text.Length;
+    }
+    private double CalculateSimilarity(IDatabase db, string text, string textKey)
+    {
+        IServer server = _redis.GetServer(_redis.GetEndPoints().First());
+
+        IEnumerable<RedisKey> keys = server.Keys(pattern: "TEXT-*");
+
+        foreach (RedisKey key in keys)
+        {
+            if (string.Compare(key, textKey) == 0)
+            {
+                continue;
+            }
+
+            if (string.Compare(text, db.StringGet(key)) == 0)
+            {
+                return 1;
+            }
+        }
+
+        return 0;
     }
 }
--- Valuator/Pages/Summary.cshtml.cs
+++ Valuator/Pages/Summary.cshtml.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace Valuator.Pages;
 public class SummaryModel : PageModel
 {
     private readonly ILogger<SummaryModel> _logger;
+    private readonly IConnectionMultiplexer _redis;
 
-    public SummaryModel(ILogger<SummaryModel> logger)
+    public SummaryModel(ILogger<SummaryModel> logger, IConnectionMultiplexer redis)
     {
         _logger = logger;
+        _redis = redis;
     }
 
     public double Rank { get; set; }
@@ -24,5 +27,18 @@ public class SummaryModel : PageModel
         _logger.LogDebug(id);
 
         // TODO: (pa1) проинициализировать свойства Rank и Similarity значениями из БД (Redis)
+        IDatabase db = _redis.GetDatabase();
+
+        RedisValue rankValue = db.StringGet($"RANK-{id}");
+        if (!string.IsNullOrWhiteSpace(rankValue))
+        {
+            Rank = (double)rankValue;
+        }
+
+        RedisValue similarityValue = db.StringGet($"SIMILARITY-{id}");
+        if (!string.IsNullOrWhiteSpace(similarityValue))
+        {
+            Similarity = (double)similarityValue;
+        }
     }
 }
