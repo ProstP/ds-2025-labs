a453644021d0e7429450a649e449dffbff4157b1
DESKTOP-K55KTC9\Prost <prosto_channal@mail.ru>
2025-03-05 10:06:19 +0300

    Добавил в запуск и выключение nginx в .cmd файлы

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,5 +1,9 @@
 @echo off
-cd ../Valuator/
+
+cd C:\D\Projects\ds-2025-labs\Valuator\
 
 start dotnet run --urls "http://0.0.0.0:5001"
 start dotnet run --urls "http://0.0.0.0:5002"
+
+cd C:\D\Projects\ds-2025-labs\nginx
+start nginx
