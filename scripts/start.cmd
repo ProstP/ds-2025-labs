0f4485ebeb73ff08e26df643d1f284d94757da46
DESKTOP-K55KTC9\Prost <prosto_channal@mail.ru>
2025-03-10 12:23:55 +0300

    Изменил путь до папок на относительные

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-cd C:\D\Projects\ds-2025-labs\Valuator\
+cd Valuator\
 
 start dotnet run --urls "http://0.0.0.0:5001"
 start dotnet run --urls "http://0.0.0.0:5002"
 
-cd C:\D\Projects\ds-2025-labs\nginx
+cd ..\nginx
 start nginx
--- scripts/stop.cmd
+++ scripts/stop.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-cd C:\D\Projects\ds-2025-labs\nginx
+cd nginx
 
 .\nginx.exe -s stop
 
-powershell -Command "Stop-Process -Id (Get-NetTCPConnection | Where-Object LocalPort -eq 5001).OwningProcess"
 powershell -Command "Stop-Process -Id (Get-NetTCPConnection | Where-Object LocalPort -eq 5002).OwningProcess"
+powershell -Command "Stop-Process -Id (Get-NetTCPConnection | Where-Object LocalPort -eq 5001).OwningProcess"
