cf41464ac621420bbdefc24d540bb0bdb51642f2
DESKTOP-K55KTC9\Prost <prosto_channal@mail.ru>
2025-05-28 17:00:31 +0300

    Удалил ненужные файлы

--- MessageBroker/IMessageBroker.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MessageBroker;
-
-public interface IMessageBroker
-{
-    public Task SendMessageAsync(string queueName, string message);
-    public Task ReceiveMessageAsync(string queueName, Action<string> messageHandler);
-}
